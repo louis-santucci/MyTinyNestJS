9c6e0bf1aeee2c242b17b8bd0c20700949f6143a
Antoine du FOU <antoine.du-fou@epita.fr>
2021-10-21 20:50:25 +0200

    [MAIN] Updated main for swagger and logger

--- nftProject/src/main.ts
+++ nftProject/src/main.ts
@@ -3,16 +3,26 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn', 'debug', 'verbose'],
+  });
 
-    const config = new DocumentBuilder()
+  const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setVersion('1.0')
+    .addBearerAuth({
+      description: 'Bearer <JWT>',
+      name: 'Authorization',
+      bearerFormat: 'Bearer',
+      scheme: 'Bearer',
+      type: 'http',
+      in: 'Header',
+    })
     .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('docs', app, document)
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
-    await app.listen(3000);
+  await app.listen(3000);
 }
 bootstrap();

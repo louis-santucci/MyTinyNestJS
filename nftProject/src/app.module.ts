8d711fd2a71e01c71b8b506cd4cefb579987004d
Antoine du FOU <antoine.du-fou@epita.fr>
2021-10-21 20:53:26 +0200

    [APP MODULE] Updated app module.ts

--- nftProject/src/app.module.ts
+++ nftProject/src/app.module.ts
@@ -6,11 +6,20 @@ import { TeamModule } from './Teams/team.module';
 import { SaleModule } from './Sales/sale.module';
 import { NftModule } from './NFTs/nft.module';
 import { NftCollectionModule } from './NFTCollections/nftcollection.module';
-
+import { AuthModule } from './Auth/auth.module';
+import { PrismaModule } from './Prisma/prisma.module';
 
 @Module({
-    imports: [UserModule, TeamModule, SaleModule, NftModule, NftCollectionModule],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    UserModule,
+    TeamModule,
+    SaleModule,
+    NftModule,
+    NftCollectionModule,
+    AuthModule,
+    PrismaModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}

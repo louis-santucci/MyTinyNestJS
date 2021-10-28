785f6ad73c22af54beaa07c0ef64ce7cdecfdc59
Antoine du FOU <antoine.du-fou@epita.fr>
2021-10-28 23:07:50 +0200

    [APP] Updating basic controller for helloworld

--- nftProject/src/app.controller.ts
+++ nftProject/src/app.controller.ts
@@ -4,36 +4,16 @@ import { ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-    @Get("/")
-    @ApiOperation({summary: ''})
-    /*@ApiQuery({
-        name: 'limit',
-        required: false,
-        schema: {
-        type: 'interger',
-        default: DEFAULT_LIMIT,
-        minimum: 0
-        },
-        description: 'the number of plant returned'
-    })
-    @ApiQuery({
-        name: 'offset',
-        required: false,
-        schema: {
-        type: 'interger',
-        default: DEFAULT_OFFSET,
-        maximum: 1000
-        },
-        description: 'the offset'
-    })*/
-    @ApiResponse({
-        status: 200,
-        description: 'Says hello',
-        type: [String]
-    })
-    getHello(): string {
-        return this.appService.getHello();
-    }
+  @Get('/')
+  @ApiOperation({ summary: 'Basic Hello World !' })
+  @ApiResponse({
+    status: 200,
+    description: 'Says hello',
+    type: [String],
+  })
+  getHello(): string {
+    return this.appService.getHello();
+  }
 }
--- nftProject/src/app.service.ts
+++ nftProject/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello! This is our project.';
-    }
+  getHello(): string {
+    return 'Hello! This is our project.';
+  }
 }

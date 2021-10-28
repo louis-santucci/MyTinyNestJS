25832ecec9ee5639dd1283084f1cb28eb51cda83
Antoine du FOU <antoine.du-fou@epita.fr>
2021-10-28 21:41:53 +0200

    [NFTCOLLECTION] Updated nft collection service and controller for image uploading, cleaning functions

--- nftProject/src/NFTCollections/nftcollection.controller.ts
+++ nftProject/src/NFTCollections/nftcollection.controller.ts
@@ -1,60 +1,133 @@
-import { Controller, UseGuards, Request, Param, Post, Body, Get } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Request,
+  Param,
+  Post,
+  Body,
+  Get,
+  UseInterceptors,
+  ValidationPipe,
+  UploadedFile,
+} from '@nestjs/common';
 import { NftCollectionService } from './nftcollection.service';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { diskStorage } from 'multer';
 import { ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/Auth/jwt.auth.guard';
 import { FindOneParams } from 'src/findOneParams';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { editFileName, imageFilter } from '../Utils/file-uploading.utils';
+import { ApiImplicitFile } from '@nestjs/swagger/dist/decorators/api-implicit-file.decorator';
+import { NftCollectionDto } from './DTO/nft-collection.dto';
 
-@ApiTags('Nftcollection')
+@ApiTags('NFT Collection')
 @Controller('nftcollection')
 export class NftCollectionController {
-    constructor(private readonly nftCollectionService: NftCollectionService) {}
+  constructor(private readonly nftCollectionService: NftCollectionService) {}
 
-    @Post('/')
-    @ApiOperation({summary: 'Create a NftCollection'})
-    @UseGuards(JwtAuthGuard)
-    @ApiResponse({
-        status: 200,
-        description: "Create a NftCollection if the team does'nt have a NftCollection.",
-        type: [String]
-    })
-    async createCollection(@Request() req, @Body() body) {
-        return this.nftCollectionService.createCollection(req.user.email, body);
-    }
+  @Post('/')
+  @ApiOperation({ summary: 'Create a NftCollection' })
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFilter,
+    }),
+  )
+  @ApiConsumes('multipart/form-data')
+  @ApiImplicitFile({
+    name: 'file',
+    required: true,
+  })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description:
+      "Create a NftCollection if the team doesn't have a NftCollection.",
+    type: [String],
+  })
+  @ApiBody({
+    type: NftCollectionDto,
+    description: 'The NFT Collection to create',
+  })
+  async createCollection(
+    @UploadedFile() file,
+    @Request() req,
+    @Body(ValidationPipe) body: NftCollectionDto,
+  ) {
+    return this.nftCollectionService.createCollection(
+      req.user.email,
+      body,
+      file.filename,
+    );
+  }
 
-    @Post('/addNft/:id')
-    @ApiOperation({summary: 'Add a NFT to a NftCollection'})
-    @UseGuards(JwtAuthGuard)
-    @ApiResponse({
-        status: 200,
-        description: 'Add a NFT to a NftCollection',
-        type: [Array]
-    })
-    async addNft(@Request() req, @Param() { id }: FindOneParams) {
-        return this.nftCollectionService.addNft(req.user.email, id);
-    }
+  @Post('/addNft/:id')
+  @ApiOperation({ summary: 'Add a NFT to a NftCollection' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: 'Add a NFT to a NftCollection',
+    type: [Array],
+  })
+  async addNft(@Request() req, @Param() { id }: FindOneParams) {
+    return this.nftCollectionService.addNft(req.user.email, id);
+  }
 
-    @Post('/deleteNft/:id')
-    @ApiOperation({summary: 'Delete a NFT to a NftCollection'})
-    @UseGuards(JwtAuthGuard)
-    @ApiResponse({
-        status: 200,
-        description: 'Delete a NFT to a NftCollection',
-        type: [Array]
-    })
-    async deleteNft(@Request() req, @Param() { id }: FindOneParams) {
-        return this.nftCollectionService.deleteNft(req.user.email, id);
-    }
+  @Post('/deleteNft/:id')
+  @ApiOperation({ summary: 'Delete a NFT to a NftCollection' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: 'Delete a NFT to a NftCollection',
+    type: [Array],
+  })
+  async deleteNft(@Request() req, @Param() { id }: FindOneParams) {
+    return this.nftCollectionService.deleteNft(req.user.email, id);
+  }
 
-    @Post('/update')
-    @ApiOperation({summary: 'Update a NftCollection'})
-    @UseGuards(JwtAuthGuard)
-    @ApiResponse({
-        status: 200,
-        description: 'Update an existing NftCollection',
-        type: [Array]
-    })
-    async updateCollection(@Request() req, @Body() body) {
-        return this.nftCollectionService.updateCollection(req.user.email, body);
-    }
+  @Post('/update')
+  @ApiOperation({ summary: 'Update a NftCollection' })
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: 200,
+    description: 'Update an existing NftCollection',
+    type: [Array],
+  })
+  @UseInterceptors(
+    FileInterceptor('image', {
+      storage: diskStorage({
+        destination: './files',
+        filename: editFileName,
+      }),
+      fileFilter: imageFilter,
+    }),
+  )
+  @ApiConsumes('multipart/form-data')
+  @ApiImplicitFile({
+    name: 'file',
+    required: true,
+  })
+  @ApiBody({
+    type: NftCollectionDto,
+    description: 'The modified collection',
+  })
+  async updateCollection(
+    @UploadedFile() file,
+    @Request() req,
+    @Body(ValidationPipe) body: NftCollectionDto,
+  ) {
+    return this.nftCollectionService.updateCollection(
+      req.user.email,
+      body,
+      file.filename,
+    );
+  }
 }
--- nftProject/src/NFTCollections/nftcollection.service.ts
+++ nftProject/src/NFTCollections/nftcollection.service.ts
@@ -1,205 +1,220 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { PrismaService } from "../Prisma/prisma.service";
-import { JwtService } from "@nestjs/jwt";
-import { NftCollectionDTO } from './DTO/nftcollection.dto';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
+import { PrismaService } from '../Prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { NftCollectionDto } from './DTO/nft-collection.dto';
 import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from 'constants';
 
 @Injectable()
 export class NftCollectionService {
-    constructor(
-        private prismaService: PrismaService,
-        private jwtService: JwtService,
-    ) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    private readonly logger = new Logger(NftCollectionService.name);
+  private readonly logger = new Logger(NftCollectionService.name);
 
-    async rate(id: number) {
-        const nfts = await this.prismaService.nft.findMany({
-            where: {
-                nftCollectionId: id,
-            },
+  async rate(id: number) {
+    const nfts = await this.prismaService.nft.findMany({
+      where: {
+        nftCollectionId: id,
+      },
+    });
+
+    if (nfts === null || nfts.length === 0) {
+      return 0;
+    }
+
+    let sum = 0;
+    for (let i = 0; i < nfts.length; i++) {
+      sum += nfts[i].rate;
+    }
+
+    return sum / nfts.length;
+  }
+
+  async createCollection(
+    useEmail: string,
+    body: NftCollectionDto,
+    filename: string,
+  ) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: useEmail,
+      },
+    });
+
+    if (user.teamId === null) {
+      throw new HttpException(
+        "You can not create a NFT Collection, you don't have a team.",
+        HttpStatus.FORBIDDEN,
+      );
+    }
+
+    const collection = await this.prismaService.nftCollection.findUnique({
+      where: {
+        teamId: user.teamId,
+      },
+    });
+
+    if (collection !== null) {
+      try {
+        return this.prismaService.nftCollection.create({
+          data: {
+            name: body.name,
+            logo: filename,
+            status: body.status,
+            teamId: user.teamId,
+            rate: 0,
+          },
         });
+      } catch (e) {
+        this.logger.error('Error creating a NFTCollection', e);
+      }
+    }
+    throw new HttpException(
+      'Your Team already have a NFT Collection.',
+      HttpStatus.FORBIDDEN,
+    );
+  }
 
-        if (nfts === null || nfts.length === 0) {
-            return 0;
-        }
+  async addNft(useEmail: string, nftId: number) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email: useEmail,
+        },
+      });
 
-        var sum = 0;
-        for (var i = 0; i < nfts.length; i++) {
-            sum += nfts[i].rate;
-        }
-        
-        return sum / nfts.length;
+      if (user.teamId === null) {
+        return "You can not add a NFT to a NFT Collection, you don't have a team.";
+      }
+
+      const nftCollection = await this.prismaService.nftCollection.findUnique({
+        where: {
+          teamId: user.teamId,
+        },
+      });
+
+      if (nftCollection === null) {
+        return 'Your team needs a NFT Collection to add a NFT.';
+      }
+
+      await this.prismaService.nft.update({
+        where: {
+          id: Number(nftId),
+        },
+        data: {
+          nftCollectionId: nftCollection.id,
+        },
+      });
+
+      const newRate = await this.rate(nftCollection.id);
+      await this.prismaService.nftCollection.update({
+        where: {
+          id: nftCollection.id,
+        },
+        data: {
+          rate: newRate,
+        },
+      });
+
+      return 'The NFT has been added.';
+    } catch (e) {
+      this.logger.error('Error Adding a NFT to NFTCollection', e);
+    }
+  }
+
+  async deleteNft(useEmail: string, nftId: number) {
+    try {
+      const user = await this.prismaService.user.findUnique({
+        where: {
+          email: useEmail,
+        },
+      });
+
+      if (user.teamId === null) {
+        return "You can not delete a NFT of a NFT Collection, you don't have a team.";
+      }
+
+      const nftCollection = await this.prismaService.nftCollection.findUnique({
+        where: {
+          teamId: user.teamId,
+        },
+      });
+
+      if (nftCollection === null) {
+        return 'Your team needs a NFT Collection to delete a NFT.';
+      }
+
+      await this.prismaService.nft.update({
+        where: {
+          id: Number(nftId),
+        },
+        data: {
+          nftCollectionId: null,
+        },
+      });
+
+      const newRate = await this.rate(nftCollection.id);
+      await this.prismaService.nftCollection.update({
+        where: {
+          id: nftCollection.id,
+        },
+        data: {
+          rate: newRate,
+        },
+      });
+
+      return 'The NFT has been deleted.';
+    } catch (e) {
+      this.logger.error('Error Delete a NFT to NFTCollection', e);
+    }
+  }
+
+  async updateCollection(
+    useEmail: string,
+    body: NftCollectionDto,
+    filename: string,
+  ) {
+    const user = await this.prismaService.user.findUnique({
+      where: {
+        email: useEmail,
+      },
+    });
+
+    if (user.teamId === null) {
+      throw new HttpException(
+        "You can not create a NFT Collection, you don't have a team.",
+        HttpStatus.FORBIDDEN,
+      );
     }
 
-    async createCollection(useEmail: string, body: NftCollectionDTO) {
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: {
-                    email: useEmail,
-                },
-            });
+    const collection = await this.prismaService.nftCollection.findUnique({
+      where: {
+        teamId: user.teamId,
+      },
+    });
 
-            if (user.teamId === null) {
-                return "You can not create a NFT Collection, you don't have a team."
-            }
-
-            const collection = await this.prismaService.nftCollection.findUnique({
-                where: {
-                    teamId: user.teamId,
-                },
-            });
-
-            if (collection === null) {
-                await this.prismaService.nftCollection.create({
-                    data: {
-                        name: body.name,
-                        logo: body.logo,
-                        status: body.status,
-                        teamId: user.teamId,
-                        rate: 0,
-                    },
-                });
-
-                return "Your Collection has been created."
-            }
-
-            return "Your Team already have a NFT Collection."
-        } catch (e) {
-            this.logger.error('Error creating a NFTCollection', e);
-        }
+    if (collection.teamId !== null) {
+      try {
+        return this.prismaService.nftCollection.update({
+          where: {
+            id: collection.id,
+          },
+          data: {
+            name: body.name,
+            logo: filename,
+            status: body.status,
+          },
+        });
+      } catch (e) {
+        this.logger.error('Error Update a NFTCollection', e);
+      }
     }
 
-    async addNft(useEmail: string, nftId: number) {
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: {
-                    email: useEmail,
-                },
-            });
-
-            if (user.teamId === null) {
-                return "You can not add a NFT to a NFT Collection, you don't have a team."
-            }
-
-            const nftCollection = await this.prismaService.nftCollection.findUnique({
-                where: {
-                    teamId: user.teamId,
-                },
-            });
-
-            if (nftCollection === null) {
-                return "Your team needs a NFT Collection to add a NFT.";
-            }
-
-            await this.prismaService.nft.update({
-                where: {
-                    id: Number(nftId)
-                },
-                data: {
-                    nftCollectionId: nftCollection.id,
-                },
-            });
-
-            const newRate = await this.rate(nftCollection.id);
-            await this.prismaService.nftCollection.update({
-                where: {
-                    id: nftCollection.id
-                },
-                data: {
-                    rate: newRate,
-                },
-            });
-
-            return "The NFT has been added."
-        } catch (e) {
-            this.logger.error('Error Adding a NFT to NFTCollection', e);
-        }
-    }
-
-    async deleteNft(useEmail: string, nftId: number) {
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: {
-                    email: useEmail,
-                },
-            });
-
-            if (user.teamId === null) {
-                return "You can not delete a NFT of a NFT Collection, you don't have a team."
-            }
-
-            const nftCollection = await this.prismaService.nftCollection.findUnique({
-                where: {
-                    teamId: user.teamId,
-                },
-            });
-
-            if (nftCollection === null) {
-                return "Your team needs a NFT Collection to delete a NFT.";
-            }
-
-            await this.prismaService.nft.update({
-                where: {
-                    id: Number(nftId)
-                },
-                data: {
-                    nftCollectionId: null,
-                },
-            });
-
-            const newRate = await this.rate(nftCollection.id);
-            await this.prismaService.nftCollection.update({
-                where: {
-                    id: nftCollection.id
-                },
-                data: {
-                    rate: newRate,
-                },
-            });
-
-            return "The NFT has been deleted."
-        } catch (e) {
-            this.logger.error('Error Delete a NFT to NFTCollection', e);
-        }
-    }
-
-    async updateCollection(useEmail: string, body: NftCollectionDTO) {
-        try {
-            const user = await this.prismaService.user.findUnique({
-                where: {
-                    email: useEmail,
-                },
-            });
-
-            if (user.teamId === null) {
-                return "You can not update a NFT Collection, you don't have a team."
-            }
-
-            const collection = await this.prismaService.nftCollection.findUnique({
-                where: {
-                    teamId: user.teamId,
-                },
-            });
-
-            if (collection.teamId !== null) {
-                await this.prismaService.nftCollection.update({
-                    where: {
-                        id: collection.id
-                    },
-                    data: {
-                        name: body.name,
-                        logo: body.logo,
-                        status: body.status,
-                    },
-                });
-
-                return "Your Collection has been updated."
-            }
-
-            return "Your team needs a NFT Collection before updating."
-        } catch (e) {
-            this.logger.error('Error Update a NFTCollection', e);
-        }
-    }
+    throw new HttpException(
+      'Your team needs a NFT Collection before updating.',
+      HttpStatus.FORBIDDEN,
+    );
+  }
 }

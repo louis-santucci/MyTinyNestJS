d8cafc5fbd34bc529bfe1573b896a8d63109cc0e
Antoine du FOU <antoine.du-fou@epita.fr>
2021-10-29 22:07:51 +0200

    [UTILS] Added FindOneParams for ids

--- nftProject/src/Utils/findOneParams.ts
+++ nftProject/src/Utils/findOneParams.ts
@@ -1,8 +1,9 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, Min } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class FindOneParams {
   @IsNumber()
+  @Min(0)
   @Transform(({ value }) => Number(value))
   id: number;
 }
